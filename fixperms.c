ac8fb28fc70371913363ff4e742cc79a390fae4c
sherpya <sherpya@netfarm.it>
2012-02-11 06:36:35 +0100

    added more field to parse

--- fixperms.c
+++ fixperms.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <limits.h>
 #include <expat.h>
 
 #define PACKAGES "packages.xml"
+//#define PACKAGES "/data/system/packages.xml"
 
 static char *blacklist[] =
 {
@@ -16,23 +18,60 @@ static char *blacklist[] =
 typedef struct _APackage
 {
     char name[PATH_MAX];
+    char realName[PATH_MAX];
     char codePath[PATH_MAX];
+
+    time_t ft;      /* timeStamp (hex) */
+    time_t it;      /* firstInstallTime (hex) */
+    time_t ut;      /* lastUpdateTime (hex) */
+
+    int version;    /* versionCode */
+
+    char resourcePath[PATH_MAX];
+    char nativeLibraryPath[PATH_MAX];
+
     int userId;
     int sharedUserId;
-    int shared;
-    int skip;
+
+    int flags;      /* pkgFlags */
+
+    bool system;
+    bool shared;
+    bool skip;
 } APackage;
 
-static int blCheck(const char *codePath)
+static bool blCheck(const char *codePath)
 {
     int i;
     for (i = 0; blacklist[i]; i++)
     {
         if (strstr(codePath, blacklist[i]))
-            return 1;
+            return true;
     }
-    return 0;
+    return false;
 }
+
+static void dumpPackage(APackage *package)
+{
+    printf("[%s] codePath:%s ", package->name, package->codePath);
+
+    if (package->shared)
+        printf("sharedUserId:%d ", package->sharedUserId);
+    else
+        printf("userId:%d ", package->userId);
+
+    printf("version:%d ", package->version);
+    printf("flags:%d ", package->flags);
+    printf("\n");
+}
+
+#define TYPE_string(var) strncpy(package->var, attrs[i + 1], sizeof(package->var))
+#define TYPE_time(var) package->var = strtoull(attrs[i + 1], NULL, 16)
+#define TYPE_int(var) package->var = atoi(attrs[i + 1])
+
+#define IFATTR(name, datatype) \
+     if (!strcmp(attrs[i], #name)) \
+        TYPE_##datatype(name);
 
 static void startElement(void *userData, const XML_Char *name, const XML_Char **attrs)
 {
@@ -44,26 +83,26 @@ static void startElement(void *userData, const XML_Char *name, const XML_Char **
 
     package = (APackage *) userData;
     memset(package, 0, sizeof(*package));
+    package->userId = package->sharedUserId = -1;
 
     for (i = 0; attrs[i]; i += 2)
     {
-        if (!strcmp(attrs[i], "name"))
-            strncpy(package->name, attrs[i + 1], sizeof(package->name));
-        else if (!strcmp(attrs[i], "codePath"))
-        {
-            strncpy(package->codePath, attrs[i + 1], sizeof(package->codePath));
-            if (!strncmp(package->codePath, "/system/app/", 12))
-                package->skip = 1;
-            else
-                package->skip = blCheck(package->codePath);
-        }
-        else if (!strcmp(attrs[i], "userId"))
-            package->userId = atoi(attrs[i + 1]);
-        else if (!strcmp(attrs[i], "sharedUserId"))
-        {
-            package->sharedUserId = atoi(attrs[i + 1]);
-            package->shared = 1;
-        }
+        IFATTR(name, string)
+        else IFATTR(realName, string)
+        else IFATTR(codePath, string)
+        else IFATTR(ft, time)
+        else IFATTR(it, time)
+        else IFATTR(ut, time)
+        else IFATTR(version, int)
+        else IFATTR(resourcePath, string)
+        else IFATTR(nativeLibraryPath, string)
+        else IFATTR(userId, int)
+        else IFATTR(sharedUserId, int)
+        else IFATTR(flags, int)
+
+        package->skip = blCheck(package->codePath);
+        package->shared = (package->sharedUserId != -1);
+        package->system = !strncmp(package->codePath, "/system/app/", 12);
     }
 }
 
@@ -73,18 +112,7 @@ static void endElement(void *userData, const XML_Char *name)
         return;
 
     APackage *package = (APackage *) userData;
-    if (package->skip)
-    {
-        //printf("Skipping %s\n", package->codePath);
-        return;
-    }
-
-    printf("[%s] codePath: %s ", package->name, package->codePath);
-    if (package->shared)
-        printf("sharedUserId: %d", package->sharedUserId);
-    else
-        printf("userId: %d", package->userId);
-    printf("\n");
+    dumpPackage(package);
 }
 
 int main(void)
